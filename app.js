9536b9b76ad5f151dff806a43412aff46bb8d207
Richard <ricky.cary1@gmail.com>
2022-10-21 18:59:03 -0500

    Ditto Added

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
+const getPokemon = () => {
+    $.ajax("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then((data) => {
+        console.log(data)
+    })
+}
+
+getPokemon()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // $.ajax('https://pokeapi.co/api/v2/pokemon/')
 // .then((data) => {
 //     console.log(data)
 //     console.log(data.results[0])
 // })
 
-$.ajax('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-.then((data) => {
-    console.log(data)
-    console.log(data.results[3])
-    console.log(data.results[4])
-    console.log(data.results[5])
-})
+// const name = document.querySelector("pokemonName").value
+
+// $.ajax('https://pokeapi.co/api/v2/pokemon/${name}')
+// .then((data) => {
+//     console.log('charmander')
+//     // console.log(data.results)
+// })
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Pokémon Pokédex
+
+A personal Pokedex used for finding Pokemon and what generation they are 
