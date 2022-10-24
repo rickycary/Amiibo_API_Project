8d873f86a50646ddff532a75b70244507d955832
Richard <ricky.cary1@gmail.com>
2022-10-24 16:54:24 -0500

    Cannot get amiibos to display out of array without placement

--- practice.js
+++ practice.js
@@ -1,6 +1,23 @@
-$.ajax("https://www.amiiboapi.com/api/amiibo/")
+const addSquare = (randomAmiibo) => {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    square.innerHTML = `<h4>${randomAmiibo}</h4>`
+    const container = document.querySelector(".amiiboContainer")
+    container.append(square)
+}
+
+const getRandomAmiibo = () => {
+    $.ajax("https://www.amiiboapi.com/api/amiibo/")
 .then((data) => {
     console.log(data)
     console.log(data.amiibo[0].character)
+    addSquare(data.amiibo[0].character)
+    addSquare(data.amiibo[99].character)
+
 })
+}
+
+getRandomAmiibo()
+const button = document.querySelector("button")
+button.addEventListener("click", getRandomAmiibo)
 
