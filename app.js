3715a99306f29895da3df4116a0b65182573ab52
Richard <ricky.cary1@gmail.com>
2022-10-25 09:41:49 -0500

    Changes made to CSS image

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+// Link to api webstie
 // https://amiiboapi.com/
 
+// Base URL
 const baseURL = 'https://www.amiiboapi.com/api'
 
-
+//====================================================================
 // Function that does the Pokedex search
 function getAmiibo(title){
     const url = $.ajax(`${baseURL}/amiibo/?name=${title}`)
 
-// Make our request
+// Make our request 
     .then((amiibo) => {
         console.log(amiibo)
 
@@ -17,14 +19,36 @@ function getAmiibo(title){
         $main.html(`
         <h1>Character Name: ${amiibo.amiibo[0].name}</h1>
         <img src="${amiibo.amiibo[0].image}">
-        <h1>Character Name: ${amiibo.amiibo[0].gameSeries}</h1>
-        <h1>Character Name: ${amiibo.amiibo[0].amiiboSeries}</h1>
-
-
-
+        <h3>Game Series: ${amiibo.amiibo[0].gameSeries}</h3>
+        <h3>Amiibo Series: ${amiibo.amiibo[0].amiiboSeries}</h3>
         `)
     })
 }
+//====================================================================
+
+
+
+//====================================================================
+// Random Amiibo Function
+const addSquare = (randomAmiibo) => {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    square.innerHTML = `<h4>${randomAmiibo}</h4>`
+    const container = document.querySelector(".amiiboContainer")
+    container.append(square)
+}
+
+const getRandomAmiibo = (random) => {
+    $.ajax(`${baseURL}/api/amiibo/`)
+.then((data) => {
+    // console.log(data)
+    console.log(data.amiibo[0].character)
+    addSquare(data.amiibo[0].character)
+    addSquare(data.amiibo[99].character)
+
+})
+}
+//====================================================================
 
 // Put a click event on the submit button
 $("input[type=submit]").on("click", event => {
