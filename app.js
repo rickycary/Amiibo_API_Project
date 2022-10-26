e15b370084c4d7c61a067613ec890bb9b22b1668
Richard <ricky.cary1@gmail.com>
2022-10-26 14:25:57 -0500

    Made code look neater

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 // Link to api webstie
 // https://amiiboapi.com/
 
+
 // Base URL
 const baseURL = 'https://www.amiiboapi.com/api'
 
+
 //====================================================================
-// Function that does the Pokedex search
+// Function that does the Amiibo search
 function getAmiibo(title){
+// Used baseURL and inserted ${title} for character
     const url = $.ajax(`${baseURL}/amiibo/?name=${title}`)
 
-// Make our request 
+// Makes our request 
     .then((amiibo) => {
         console.log(amiibo)
 
         const $main = $('main')
         $main.empty()
 
+// Information to translate to the page from the API
         $main.html(`
         <h1>Character Name: ${amiibo.amiibo[0].name}</h1>
         <img src="${amiibo.amiibo[0].image}">
@@ -26,71 +30,25 @@ function getAmiibo(title){
 }
 //====================================================================
 
+
 // Put a click event on the submit button
 $("input[type=submit]").on("click", event => {
 
+
+// Button to prevent refresh
     event.preventDefault()
 
+
+// Gives input text value
     const inputText = $("input[type=text]").val()
 
+
+// Call to place input text
     getAmiibo(inputText)
 })
 
-
-getAmiibo('link')
-
-
-
-//====================================================================
-// Random Amiibo Function
-// const addSquare = (randomAmiibo) => {
-//     const square = document.createElement('div')
-//     square.classList.add('square')
-//     square.innerHTML = `<h4>${randomAmiibo}</h4>`
-//     const container = document.querySelector(".amiiboContainer")
-//     container.append(square)
-// }
-
-// const getRandomAmiibo = (random) => {
-//     $.ajax(`${baseURL}/api/amiibo/`)
-// .then((data) => {
-//     // console.log(data)
-//     console.log(data.amiibo[0].character)
-//     addSquare(amiibo[0].name)
-// })
-// }
-
-// getRandomAmiibo()
-// const button = document.querySelector("button")
-// button.addEventListener("click", getRandomAmiibo)
-
-//====================================================================
+// Call for character to appear on screen 
+getAmiibo(' ')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $.ajax('https://pokeapi.co/api/v2/pokemon/')
-// .then((data) => {
-//     console.log(data)
-//     console.log(data.results[0])
-// })
-
-// const name = document.querySelector("pokemonName").value
-
-// $.ajax('https://pokeapi.co/api/v2/pokemon/${name}')
-// .then((data) => {
-//     console.log('charmander')
-//     // console.log(data.results)
-// })
